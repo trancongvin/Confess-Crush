e0e7cc6030ec18b8e367f93e8eecfa8bb5651f1a
trancongvin <94386524+trancongvin@users.noreply.github.com>
2021-11-16 08:29:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "cin chàoooo",
+    introTitle: 'em bé dưa hấuuuu',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao dưa cứ đi nhầm đường
+    Lạc vào tim su lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Phải chăng dưa đã yêu sukemmm ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng dưa đã say skemm ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'xạ đúng gòi <33',
+    btnNo: 'mơ đi cưng :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao dưa lại yêu skemm <3',
+    btnReply: 'Gửi choa su bíuu <3',
+    reply: 'Do su kem đẹp trai siêu cấp vjp proo',
+    mess: 'suuu biết mà 🥰. Yêu dưa nhiều nhiều 😘😘',
+    messDesc: 'Tí nữaaa, mình coi doraemonnnnn nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: '  https://toplist.vn/top-list/tap-phim-doraemon-movie-hay-nhat-ban-nen-xem-thu-16939.htm ' //link mess của các bạn. VD: https://m.me/nam.nodemy
